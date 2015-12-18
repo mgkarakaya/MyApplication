3ae651e2b50a200baa919da476ab6cada24f0362
mgkarakaya <mgkarakaya@gmail.com>
2015-12-18 17:51:15 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/test/myapplication/MainActivity.java
+++ app/src/main/java/com/example/test/myapplication/MainActivity.java
@@ -1,19 +1,124 @@
-package com.example.test.myapplication;
+package com.example.mgk.readingcontacts;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
+import java.util.ArrayList;
 
 
-public class MainActivity extends ActionBarActivity {
+public class FetchContacts extends AppCompatActivity {
+public TextView outputText;
 
+Button uploadButton=(Button)findViewById(R.id.uploadButton);
+
+
+    ArrayList<String> names_and_numbers = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        outputText = (TextView) findViewById(R.id.textView1);
+        fetchContacts();
 
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+    public void fetchContacts() {
+
+        String phoneNumber = null;
+        String email = null;
+
+        Uri CONTENT_URI = ContactsContract.Contacts.CONTENT_URI;
+        String _ID = ContactsContract.Contacts._ID;
+        String DISPLAY_NAME = ContactsContract.Contacts.DISPLAY_NAME;
+        String HAS_PHONE_NUMBER = ContactsContract.Contacts.HAS_PHONE_NUMBER;
+
+        Uri PhoneCONTENT_URI = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
+        String Phone_CONTACT_ID = ContactsContract.CommonDataKinds.Phone.CONTACT_ID;
+        String NUMBER = ContactsContract.CommonDataKinds.Phone.NUMBER;
+
+        Uri EmailCONTENT_URI =  ContactsContract.CommonDataKinds.Email.CONTENT_URI;
+        String EmailCONTACT_ID = ContactsContract.CommonDataKinds.Email.CONTACT_ID;
+        String DATA = ContactsContract.CommonDataKinds.Email.DATA;
+
+
+        StringBuffer output = new StringBuffer();
+
+        ContentResolver contentResolver = getContentResolver();
+
+        Cursor cursor = contentResolver.query(CONTENT_URI, null,null, null, null);
+
+        // Loop for every contact in the phone
+        if (cursor.getCount() > 0) {
+
+            while (cursor.moveToNext()) {
+
+                String contact_id = cursor.getString(cursor.getColumnIndex( _ID ));
+                String name = cursor.getString(cursor.getColumnIndex(DISPLAY_NAME));
+
+                    output.append("\n First Name:" + name);
+                    names_and_numbers.add(name);
+                    // Query and loop for every phone number of the contact
+                    Cursor phoneCursor = contentResolver.query(PhoneCONTENT_URI, null, Phone_CONTACT_ID + " = ?", new String[] { contact_id }, null);
+
+                    while (phoneCursor.moveToNext()) {
+                        phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(NUMBER));
+                        output.append("\n Phone number:" + phoneNumber);
+                        names_and_numbers.add(phoneNumber);
+
+                    }
+
+                    phoneCursor.close();
+
+                    // Query and loop for every email of the contact
+                    Cursor emailCursor = contentResolver.query(EmailCONTENT_URI,	null, EmailCONTACT_ID+ " = ?", new String[] { contact_id }, null);
+
+                    while (emailCursor.moveToNext()) {
+
+                        email = emailCursor.getString(emailCursor.getColumnIndex(DATA));
+
+                        output.append("\nEmail:" + email);
+                        names_and_numbers.add(email);
+                    }
+
+                    emailCursor.close();
+
+
+                output.append("\n");
+
+
+            }
+            for(int i = 0; i < names_and_numbers.size(); i++) {
+                System.out.println(names_and_numbers.get(i));
+            }
+            outputText.setText(output);
+
+            }
+
+        }
+    public ArrayList<String> getlist(){
+        return names_and_numbers;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
